c10c4ab3fdddaa5cc1a019ee7e7911152f89b515
tgremlin <allantenison@live.com>
2021-12-12 10:48:58 -0600

    Footer must be pulled from different file. Created footer.tf...might move header and footer to MD files in future release.

--- main.tf
+++ main.tf
@@ -335,15 +335,3 @@ resource "aws_db_subnet_group" "selected" {
     Name = "terraform demo DB subnet group"
   }
 }
-
-/**
- * # Diagrams
- * Below is a high level diagram of the pipeline.
- * ![InfraDiagram](/images/Terraform-CircleCI-AWS-ASG.svg)
- * 
- * Below is a dependency graph.
- * ![DependencyGraph](/images/ASGgraph.svg)  
- * 
- * 
- * 
- */
